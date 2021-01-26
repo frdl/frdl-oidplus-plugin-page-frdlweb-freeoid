249a64e0add2c3436a4314dc0894e20f818dddfd
Till Wehowski <github@webfan.de>
2021-01-26 12:09:36 +0100

    Update OIDplusPagePublicFreeOIDFrdlweb.class.php
    + allow registration although email/ may be registered in another way, e.g. via oauth.

--- plugins/publicPages/200_frdlweb_freeoid/OIDplusPagePublicFreeOIDFrdlweb.class.php
+++ plugins/publicPages/200_frdlweb_freeoid/OIDplusPagePublicFreeOIDFrdlweb.class.php
@@ -36,16 +36,26 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 		if ($actionID == 'request_freeweid') {
 			$email = $params['email'];
 
+			
+			$hasFeature = $this->raHasFreeWeid($email, false);					
+			if ($hasFeature) {
+				throw new OIDplusException('Your RA is already registered for oneOf the FREE-WEID featured OIDs.');
+			}
+			/*	//$out['text'] = 'Your RA is already registered for oneOf the FREE-WEID featured OIDs.';
+			
+			
 			$res = OIDplus::db()->query("select * from ###ra where email = ?", array($email));
 			if ($res->num_rows() > 0) {
-				throw new OIDplusException('This email address already exists.'); // TODO: actually, the person might have something else (like a DOI) and want to have a FreeOID
+				 throw new OIDplusException('This email address already exists.'); // TODO: actually, the person might have something else (like a DOI) and want to have a FreeOID
 			}
-
+*/
 			if (!OIDplus::mailUtils()->validMailAddress($email)) {
 				throw new OIDplusException('Invalid email address');
 			}
-
-			if (OIDplus::baseConfig()->getValue('RECAPTCHA_ENABLED', false)) {
+			
+            $login_list = OIDplus::authUtils()->loggedInRaList();
+			
+			if (!count($login_list) && OIDplus::baseConfig()->getValue('RECAPTCHA_ENABLED', false)) {
 				$secret=OIDplus::baseConfig()->getValue('RECAPTCHA_PRIVATE', '');
 				$response=$params["captcha"];
 				$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
@@ -59,7 +69,7 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 			OIDplus::logger()->log("[INFO]OID(oid:$root_oid)+RA($email)!", "Requested a free OID for email '$email' to be placed into root '$root_oid'");
 
 			$timestamp = time();
-			$activate_url = OIDplus::getSystemUrl() . '?goto='.urlencode('oidplus:com.frdlweb.freeoid.activate_freeweid$'.$email.'$'.$timestamp.'$'.OIDplus::authUtils()::makeAuthKey('com.frdlweb.freeoid.activate_freeweid;'.$email.';'.$timestamp));
+			$activate_url = OIDplus::webpath() . '?goto='.urlencode('oidplus:com.frdlweb.freeoid.activate_freeweid$'.$email.'$'.$timestamp.'$'.OIDplus::authUtils()::makeAuthKey('com.frdlweb.freeoid.activate_freeweid;'.$email.';'.$timestamp));
 
 			$message = file_get_contents(__DIR__ . '/request_msg.tpl');
 			$message = str_replace('{{SYSTEM_URL}}', OIDplus::webpath(), $message);
@@ -87,28 +97,41 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 				throw new OIDplusException('Invalid auth key');
 			}
 
+			$login_list = OIDplus::authUtils()->loggedInRaList();
+			$ra = new OIDplusRA($email);
+			$raExists = $ra->existing();
+			$ra_logged_in =true===$raExists && OIDplus::authUtils()->isRaLoggedIn($email);
+			
+			
+			
 			if ((OIDplus::config()->getValue('max_ra_invite_time') > 0) && (time()-$timestamp > OIDplus::config()->getValue('max_ra_invite_time'))) {
 				throw new OIDplusException('Invitation expired!');
 			}
 
-			if ($password1 !== $password2) {
+			if (!$ra_logged_in && $password1 !== $password2) {
 				throw new OIDplusException('Passwords are not equal');
 			}
 
-			if (strlen($password1) < OIDplus::config()->getValue('ra_min_password_length')) {
+			if (!$ra_logged_in && strlen($password1) < OIDplus::config()->getValue('ra_min_password_length')) {
 				throw new OIDplusException('Password is too short. Minimum password length: '.OIDplus::config()->getValue('ra_min_password_length'));
 			}
 
-			if (empty($ra_name)) {
+			if (!$ra_logged_in && empty($ra_name)) {
 				throw new OIDplusException('Please enter your personal name or the name of your group.');
 			}
-
+			
+			if (true ===  $raExists && count($login_list) >= 10) {
+				throw new OIDplusException('You have too many root arcs registered already!');
+			}
 			// 1. step: Add the RA to the database
 
 			$ra = new OIDplusRA($email);
-			$ra->register_ra($password1);
-			$ra->setRaName($ra_name);
-
+			
+			if(false === $raExists)$ra->register_ra($password1);
+			if(false === $raExists)$ra->setRaName($ra_name);
+			
+			   $ra = new OIDplusRA($email);
+               $ra_name = $ra->raName();
 			// 2. step: Add the new OID to the database
 
 			$root_oid = self::getFreeRootOid(false);
@@ -130,7 +153,9 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 				if ('oid:'.$new_oid > OIDplus::baseConfig()->getValue('LIMITS_MAX_ID_LENGTH')) {
 					throw new OIDplusException("The resulting object identifier '$new_oid' is too long (max allowed length ".(OIDplus::baseConfig()->getValue('LIMITS_MAX_ID_LENGTH')-strlen('oid:')).")");
 				}
-
+				
+//$res = OIDplus::db()->query("select id from ###objects where ra_email = ? and id like ? order by ".OIDplus::db()->natOrder('id'), array($email, self::getFreeRootOid(true).'.%'));
+				
 				OIDplus::db()->query("insert into ###objects (id, ra_email, parent, title, description, confidential, created) values (?, ?, ?, ?, ?, ?, ".OIDplus::db()->sqlDate().")", array('oid:'.$new_oid, $email, self::getFreeRootOid(true), $title, $description, true));
 			} catch (Exception $e) {
 				$ra->delete();
@@ -177,6 +202,19 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 		
 			
 	}
+	/*
+		public function whoisObjectAttributes($id, &$out) {
+		// Interface 1.3.6.1.4.1.37476.2.5.2.3.4
+
+		$files = glob(self::getUploadDir($id).'/'.'*');
+		foreach ($files as $file) {
+			$out[] = 'attachment-name: '.basename($file);
+			$out[] = 'attachment-url: '.OIDplus::webpath(__DIR__).'download.php?id='.urlencode($id).'&filename='.urlencode(basename($file));
+		}
+
+	}
+	public function whoisRaAttributes($email, &$out) {} // Interface 1.3.6.1.4.1.37476.2.5.2.3.4
+	*/
 	
 	
    public function modifyContent($id, &$title, &$icon, &$text){
@@ -191,7 +229,8 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 	 }catch(\Exception $e){
 		$obj = false; 
 	 }  
-		 
+//if (false!==strpos($id, 'weid:') ) {	   
+ // if (false!==$obj ) {		 
 		//die(print_r($obj,true).'<br />'.$id.'<br />'.$title.'<br />'.$text);
 	   $weidObj = false;
 	   
@@ -208,7 +247,9 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 	   $CRUD =(is_object($weidObj) && null !== $weidObj && is_callable([$weidObj, 'renderChildren'])) ? $weidObj->renderChildren($children, '<h4>Children:</h4>') : '';
 	  
 	 }
-	}  
+  }  
+//	}     
+	   
 	   $content =(false===strpos($content, '%%CRUD%%') ) ? $content.$CRUD :  str_replace('%%CRUD%%', \PHP_EOL.$CRUD.\PHP_EOL.'%%CRUD%%', $content);	   
 	  
 	   $handled = false;
@@ -225,12 +266,11 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 		//print_r($obj);
 	//}
 	
-	public function implementsFeature($id) {
-		if (strtolower($id) == '1.3.6.1.4.1.37476.2.5.2.3.2') return true; // modifyContent
-		if (strtolower($id) == '1.3.6.1.4.1.37476.2.5.2.3.4') return true; // publicPages, whoisObjectAttributes($id, &$out),  whoisRaAttributes($email, &$out)
-	//	if (strtolower($id) == '1.3.6.1.4.1.37476.2.5.2.3.3') return true; // beforeObject*, afterObject*
-		return false;
-	}
+//	public function implementsFeature($id) {
+	//	if (strtolower($id) == '1.3.6.1.4.1.37476.2.5.2.3.2') return true; // modifyContent
+	//	if (strtolower($id) == '1.3.6.1.4.1.37476.2.5.2.3.4') return true; // publicPages, whoisObjectAttributes($id, &$out),  whoisRaAttributes($email, &$out)
+//		return false;
+//	}
 	
 
 	public function gui($id, &$out, &$handled) {
@@ -247,29 +287,68 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 			$out['text'] .= '<p>Currently <a '.OIDplus::gui()->link(self::getFreeRootOid(true)).'>'.$highest_id.' free OIDs have been</a> registered. Please enter your email below to receive a free OID.</p>';
 
 			try {
+				
+				
+			$hasFeature = false;	
+			$login_list = OIDplus::authUtils()->loggedInRaList();
+				$logedin_with_emails = [];
+				$logedin_email = '';
+			if (count($login_list) > 0) {
+				foreach ($login_list as $x) {
+					//$tabcont .= '<p>'._L('You are logged in as %1','<b>'.$x->raEmail().'</b>').' (<a href="#" onclick="return raLogout('.js_escape($x->raEmail()).');">'._L('Logout').'</a>)</p>';
+					$logedin_with_emails[$x->raEmail()] = '<button onclick="document.getElementById(\'email\').value=\''.$x->raEmail().'\';return false;">'.$x->raEmail().'</button>';
+					$logedin_email = $x->raEmail();
+					if($this->raHasFreeWeid($x->raEmail(), false)){
+						$hasFeature = true;
+					}
+				}
+				//$tabcont .= '<p>'._L('If you have more accounts, you can log in with another account here.').'</p>';
+			} else {
+				//$tabcont .= '<p>'._L('Enter your email address and your password to log in as Registration Authority.').'</p>';
+			}		
+				
+				
+			//$ra = new OIDplusRA($email);
+			//$raExists = $ra->existing();
+			//$ra_logged_in =true===$raExists && OIDplus::authUtils()->isRaLoggedIn($email);	
+				
+				if(false !== $hasFeature){					
+					$out['text'] = 'Your RA is already registered for oneOf the FREE-WEID featured OIDs.'; // TODO: actually, the person might have something else (like a DOI) and want to have a FreeOID
+				}else{
 				$out['text'] .= '
 				  <form id="freeOIDForm" onsubmit="return freeWEIDFormOnSubmit();">
-				    E-Mail: <input type="text" id="email" value=""/><br><br>'.
-				 (OIDplus::baseConfig()->getValue('RECAPTCHA_ENABLED', false) ?
+				    E-Mail: <input type="text" id="email" value="'.$logedin_email.'"/><br><br>'.
+				 (!count($login_list) && OIDplus::baseConfig()->getValue('RECAPTCHA_ENABLED', false) ?
 				 '<script> grecaptcha.render(document.getElementById("g-recaptcha"), { "sitekey" : "'.OIDplus::baseConfig()->getValue('RECAPTCHA_PUBLIC', '').'" }); </script>'.
 				 '<div id="g-recaptcha" class="g-recaptcha" data-sitekey="'.OIDplus::baseConfig()->getValue('RECAPTCHA_PUBLIC', '').'"></div>' : '').
 				' <br>
 				    <input type="submit" value="Request free OID">
 				  </form>';
-
+				}
+				
+				
 				$obj = OIDplusOID::parse(self::getFreeRootOid(true));
 
-				$tos = file_get_contents(__DIR__ . '/tos.html');
+			//	$tos = file_get_contents(__DIR__ . '/tos.html');
+							
+				if (file_exists(__DIR__ . '/tos$'.OIDplus::getCurrentLang().'.html')) {
+					$tos = file_get_contents(__DIR__ . '/tos$'.OIDplus::getCurrentLang().'.html');
+				} else {
+					$tos = file_get_contents(__DIR__ . '/tos.html');
+				}
+
 				$tos = str_replace('{{ADMIN_EMAIL}}', OIDplus::config()->getValue('admin_email'), $tos);
 				if ($obj) {
 					$tos = str_replace('{{ROOT_OID}}', $obj->getDotNotation(), $tos);
 					$tos = str_replace('{{ROOT_OID_ASN1}}', $obj->getAsn1Notation(), $tos);
 					$tos = str_replace('{{ROOT_OID_IRI}}', $obj->getIriNotation(), $tos);
 				}
-				$out['text'] .= $tos;
+				
+					$out['text'] .= $tos;
 			} catch (Exception $e) {
 				$out['text'] = "Error: ".$e->getMessage();
 			}
+				
 		} else if (explode('$',$id)[0] == 'oidplus:com.frdlweb.freeoid.activate_freeweid') {
 			$handled = true;
 
@@ -280,11 +359,31 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 			$out['title'] = 'Activate Free OID/WEID';
 			$out['icon'] = file_exists(__DIR__.'/icon_big.png') ? OIDplus::webpath(__DIR__).'icon_big.png' : '';
 
+			
+			
+			
+			$login_list = OIDplus::authUtils()->loggedInRaList();                      
+			$ra = new OIDplusRA($email);
+			$raExists = $ra->existing();
+			$ra_logged_in =true===$raExists && OIDplus::authUtils()->isRaLoggedIn($email);
+			
+			
+			$hasFeature = $this->raHasFreeWeid($email, false);					
+		//	if ($hasFeature) {
+		//		throw new OIDplusException('Your RA is already registered for oneOf the FREE-WEID featured OIDs.');
+		//	}
+			
 			$res = OIDplus::db()->query("select * from ###ra where email = ?", array($email));
-			if ($res->num_rows() > 0) {
+			/*
+			if (count($login_list) > 10 && $res->num_rows() > 0) {
 				$out['icon'] = 'img/error_big.png';
 				$out['text'] = 'This RA is already registered.'; // TODO: actually, the person might have something else (like a DOI) and want to have a FreeOID
-			} else {
+			}
+			*/
+			if($hasFeature){
+				$out['icon'] = 'img/error_big.png';
+				$out['text'] = 'This RA is already registered for oneOf the FREE-WEID featured OIDs.'; 				
+			}else {
 				if (!OIDplus::authUtils()::validateAuthKey('com.frdlweb.freeoid.activate_freeweid;'.$email.';'.$timestamp, $auth)) {
 					$out['icon'] = 'img/error_big.png';
 					$out['text'] = 'Invalid authorization. Is the URL OK?';
@@ -299,9 +398,16 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 				    Your personal name or the name of your group:<br><input type="text" id="ra_name" value=""/><br><br><!-- TODO: disable autocomplete -->
 				    Title of your OID (usually equal to your name, optional):<br><input type="text" id="title" value=""/><br><br>
 				    URL for more information about your project(s) (optional):<br><input type="text" id="url" value=""/><br><br>
-
+                  '.
+				((!count($login_list)) ?	
+						
+				    '		
 				    <div><label class="padding_label">Password:</label><input type="password" id="password1" value=""/></div>
 				    <div><label class="padding_label">Repeat:</label><input type="password" id="password2" value=""/></div>
+					'
+				 : '' ).
+					
+					'
 				    <br><input type="submit" value="Register">
 				  </form>';
 				}
@@ -326,14 +432,26 @@ class OIDplusPagePublicFreeOIDFrdlweb extends OIDplusPagePublicObjects {
 		$json[] = array(
 			'id' => 'com.frdlweb.freeweid',
 			'icon' => $tree_icon,
-			'text' => 'Register a free WEID'
+			'text' =>  _L('Register a free WEID')
 		);
 
 		return true;
 	}
 
 
+/*
+$res = OIDplus::db()->query("select id from ###objects where ra_email = ? and id like ? order by ".OIDplus::db()->natOrder('id'), array($email, self::getFreeRootOid(true).'.%'));
 
+*/
+	public function raHasFreeWeid($email, $getId = false){
+		$res = OIDplus::db()->query("select id from ###objects where ra_email = ? and id like ? order by ".OIDplus::db()->natOrder('id'), array($email, self::getFreeRootOid(true).'.%'));
+		while ($row = $res->fetch_array()) {
+		   return (true === $getId) ? $row['id'] : true;	
+		}
+		
+	  return false;	
+	}
+	
 	protected static function freeoid_max_id() {
 		$res = OIDplus::db()->query("select id from ###objects where id like ? order by ".OIDplus::db()->natOrder('id'), array(self::getFreeRootOid(true).'.%'));
 		$highest_id = 0;
